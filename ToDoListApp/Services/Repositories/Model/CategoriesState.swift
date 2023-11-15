b007e1cb85afd233ed5c00c0992b2fc51549f4ec
Alice <130156538+vasilevali@users.noreply.github.com>
2023-11-15 23:18:33 +0300

    Файл удален из проекта, но изменение не подгружается

--- ToDoListApp/Services/Repositories/Model/CategoriesState.swift
+++ ToDoListApp/Services/Repositories/Model/CategoriesState.swift
@@ -1,14 +1,1 @@
-//
-//  CategoriesState.swift
-//  ToDoListApp
-//
-//  Created by мак on 14.11.2023.
-//
 
-import Foundation
-
-enum CategoriesState {
-    case loading
-    case success([TaskModel])
-    case failed
-}
